184978b32e1e82b3196d94359d4b2e61186429d4
Ryosuke-Shigi <adalovelace6365@gmail.com>
2022-07-17 17:58:49 +0900

    bulma success

---
+++ resources/views/bulma.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>bulma test</title>
+
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"> --}}
+
+</head>
+<body>
+    <a class="button is-success">Success</a>
+</body>
+</html>
